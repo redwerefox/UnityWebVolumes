3aa1a4917c2fce955b0c8bcd4a11f499faa34dd7
_Vulpes <vulpesk1tzune@gmail.com>
2026-06-05 17:04:50 +0200

    improve collider zones of the container, add angular speed clamping for stability, add little timer to not start rotations before half second has passed

--- Assets/RotateContainer.cs
+++ Assets/RotateContainer.cs
@@ -6,8 +6,16 @@ using Unity.Mathematics;
 
 public partial struct RotateContainer : ISystem
 {
+
     public void OnUpdate(ref SystemState state)
     {
+
+        if (SystemAPI.Time.ElapsedTime < 0.5)
+        {
+            return;
+        }
+
+
         float mouseInputX = Input.GetAxis("Mouse X");
         float mouseInputY = Input.GetAxis("Mouse Y");
 
@@ -15,11 +23,18 @@ public partial struct RotateContainer : ISystem
         mouseInputY *= SystemAPI.Time.DeltaTime * 1000f;
 
 
+        float maxSpinSpeed = 5.0f;
+        float spinSpeed = 2.0f;
+
         foreach (var transform in SystemAPI.Query<RefRW<PhysicsVelocity>>().WithAll<Container>())
         {
 
-            float spinSpeed = 1.0f;
             float3 angularVelocity = new float3(-mouseInputY * spinSpeed, mouseInputX * spinSpeed, 0f);
+            float angularVelocityMagnitude = math.length(angularVelocity);
+            if (angularVelocityMagnitude > maxSpinSpeed)
+            {
+                angularVelocity = math.normalize(angularVelocity) * maxSpinSpeed;
+            }
             transform.ValueRW.Angular = angularVelocity;
         }
 
